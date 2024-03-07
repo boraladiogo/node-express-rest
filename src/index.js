85654e277602a4d261c725d06cf4bd62bc8b9150
Diogo Martins <boraladiogo@gmail.com>
2024-03-07 12:33:43 -0300

    Create a basic route example

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/', (request, response) => {
+    response.send('Hello World!');
+});
+
 app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
