c7fb006995a13b306229cccef351e03ae6e817bc
Diogo Martins <boraladiogo@gmail.com>
2024-03-08 19:09:00 -0300

    Create ContactController class with basic CRUD name convention methods

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,22 @@
+class ContactController {
+    index() {
+        // Listar todos os registros
+    }
+
+    show() {
+        // Obter um único registro
+    }
+
+    store() {
+        // Criar novo registro
+    }
+
+    update() {
+        // Editar um registro
+    }
+
+    delete() {
+        // Remover um registro
+    }
+
+}
