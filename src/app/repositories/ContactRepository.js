f1f5435018e264249fbe4e17f05b49909dfad809
Diogo Martins <boraladiogo@gmail.com>
2024-03-13 19:52:32 -0300

    Create a 'contacts' array for data access demonstration

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,3 +1,22 @@
+const crypto = require('node:crypto');
+
+const contacts = [
+    {
+        id: crypto.randomUUID(),
+        name: 'Diogo',
+        email: 'diogo@email.com',
+        phone: '12346789',
+        category: crypto.randomUUID(),
+    },
+    {
+        id: crypto.randomUUID(),
+        name: 'Thais',
+        email: 'thais@email.com',
+        phone: '987654321',
+        category: crypto.randomUUID(),
+    },
+];
+
 class ContactRepository {}
 
 module.exports = new ContactRepository();
