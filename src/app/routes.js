81531144b57b94f4d7eefd7741ddce69bbb5925c
Diogo Martins <boraladiogo@gmail.com>
2024-03-16 21:28:06 -0300

    Add PUT route '/contacts/:id' to update contact data

--- src/app/routes.js
+++ src/app/routes.js
@@ -9,6 +9,8 @@ router.get('/contacts/:id', ContactController.show);
 
 router.post('/contacts', ContactController.store);
 
+router.put('/contacts/:id', ContactController.update);
+
 router.delete('/contacts/:id', ContactController.delete);
 
 module.exports = router;
