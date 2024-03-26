724fd29f2e1433f8e9f0e99bc2ad5f07b9a5553f
Diogo Martins <boraladiogo@gmail.com>
2024-03-26 19:09:25 -0300

    Create a CategoryController class to construct basic CRUD operations

---
+++ src/app/controllers/CategoryController.js
@@ -0,0 +1,23 @@
+class CategoryController {
+    index() {
+        // List all categories
+    }
+
+    show() {
+        // Get one category
+    }
+
+    store() {
+        // Create new category
+    }
+
+    update() {
+        // Update category
+    }
+
+    delete() {
+        // Delete category
+    }
+}
+
+module.exports = new CategoryController();
