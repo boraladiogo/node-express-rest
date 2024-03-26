6248ba1ae7c038d5e32ec8237b21e548f555e53c
Diogo Martins <boraladiogo@gmail.com>
2024-03-26 19:06:44 -0300

    Remove contacts mock

--- src/app/repositories/ContactRepository.js
+++ src/app/repositories/ContactRepository.js
@@ -1,22 +1,5 @@
 const database = require('../../database');
 
-const contacts = [
-    {
-        id: 'b12f2603-c07e-4614-b89e-feadf0cbcefa',
-        name: 'Diogo',
-        email: 'diogo@email.com',
-        phone: '12346789',
-        category: 'ca2d316b-d460-48bc-8834-96205733f661',
-    },
-    {
-        id: 'dc2d53e9-9b78-4b30-af31-4a8b655e2c6d',
-        name: 'Thais',
-        email: 'thais@email.com',
-        phone: '987654321',
-        category: '014f3e0d-3568-4241-bc06-f6c003b87dbd',
-    },
-];
-
 class ContactRepository {
     async find(orderBy = 'ASC') {
         const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
