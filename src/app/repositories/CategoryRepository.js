01ca780de6fac3817f022ce00cc76287382899ae
Diogo Martins <boraladiogo@gmail.com>
2024-03-26 19:19:47 -0300

    Create a CategoryRepository class to access data in a SQL database

---
+++ src/app/repositories/CategoryRepository.js
@@ -0,0 +1,19 @@
+class CategoryRepository {
+    find() {
+        // List all contacts in database
+    }
+
+    create() {
+        // Create new contact in database
+    }
+
+    update() {
+        // Update a contact in database
+    }
+
+    delete() {
+        // Delete a contact in database
+    }
+}
+
+module.exports = new CategoryRepository();
