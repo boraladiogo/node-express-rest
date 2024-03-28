91a02f967418aed57d6a010131592c70d0affd7a
Diogo Martins <boraladiogo@gmail.com>
2024-03-28 13:55:03 -0300

    Add GET route '/categories' to retrive categories data

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 
 const ContactController = require('./controllers/ContactController');
+const CategoryController = require('./controllers/CategoryController');
 
 const router = Router();
 
+// Contacts routes
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 
@@ -12,5 +14,8 @@ router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
 
 router.delete('/contacts/:id', ContactController.delete);
+
+// Categories routes
+router.get('/categories', CategoryController.index);
 
 module.exports = router;
