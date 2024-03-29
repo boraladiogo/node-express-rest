eb741373fda541ef5251c7e09059998da5db4819
Diogo Martins <boraladiogo@gmail.com>
2024-03-29 16:36:30 -0300

    Add POST route '/categories' to create a new category

--- src/app/routes.js
+++ src/app/routes.js
@@ -8,14 +8,12 @@ const router = Router();
 // Contacts routes
 router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
-
 router.post('/contacts', ContactController.store);
-
 router.put('/contacts/:id', ContactController.update);
-
 router.delete('/contacts/:id', ContactController.delete);
 
 // Categories routes
 router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
 
 module.exports = router;
